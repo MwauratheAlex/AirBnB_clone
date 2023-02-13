a2e6519df2134927c896ebca3c506a71992cbc73
MwauratheAlex <mwaurathealex@gmail.com>
2023-02-13 19:31:26 +0300

    corrections: user

--- console.py
+++ console.py
@@ -4,11 +4,24 @@ import cmd
 from models.base_model import BaseModel
 from models import storage
 from models.user import User
+from models.state import State
+from models.city import City
+from models.amenity import Amenity
+from models.place import Place
+from models.review import Review
 
 
 class HBNBCommand(cmd.Cmd):
     """ Class for the AirHBNB console """
-    __classes = {"BaseModel" : BaseModel, "User" : User}
+    __classes = {
+            "BaseModel" : BaseModel,
+            "User" : User,
+            "State" : State,
+            "City" : City,
+            "Amenity" : Amenity,
+            "Place" : Place,
+            "Review" : Review
+            }
     prompt = "(hbnb) "
 
 
