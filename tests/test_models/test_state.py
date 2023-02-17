0ead2c1644c339725a208ae25a639d2d8bcd5b65
MwauratheAlex <mwaurathealex@gmail.com>
2023-02-17 18:03:32 +0300

    add tests state

--- tests/test_models/test_state.py
+++ tests/test_models/test_state.py
@@ -1,1 +1,20 @@
 #!/usr/bin/python3
+""" This module contains the class TestState"""
+from tests.test_models.test_base_model import TestBaseModel
+from models.state import State
+
+
+class TestState(TestBaseModel):
+    """Contains tests for the class State"""
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.name = "State"
+        self.value = State
+
+    def test_has_attributes(self):
+        s = State()
+        self.assertTrue(hasattr(s, "name"))
+
+    def test_name(self):
+        s = State()
+        self.assertEqual(type(s.name), str)
