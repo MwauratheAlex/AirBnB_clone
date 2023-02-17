e962ae079c5bf84f29ca7a8af1b719adc9a99cef
MwauratheAlex <mwaurathealex@gmail.com>
2023-02-17 17:29:46 +0300

    add tests city

--- tests/test_models/test_city.py
+++ tests/test_models/test_city.py
@@ -1,1 +1,25 @@
 #!/usr/bin/python3
+"""Module contains class TestCity"""
+from tests.test_models.test_base_model import TestBaseModel
+from models.city import City
+
+
+class TestCity(TestBaseModel):
+    """Contains test for class City"""
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.name = "City"
+        self.value = "City"
+
+    def test_has_attributes(self):
+        """Test if all attributes are present"""
+        c = City()
+        self.assertTrue(hasattr(c, "state_id"))
+        self.assertTrue(hasattr(c, "name"))
+
+    def test_attr_types(self):
+        """Test if attributes are of correct type"""
+        c = City()
+        self.assertTrue(type(c.state_id), str)
+        self.assertTrue(type(c.name), str)
+
