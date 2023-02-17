1df16bb848142662ef0266916bc522abb76774ee
MwauratheAlex <mwaurathealex@gmail.com>
2023-02-17 15:49:55 +0300

    add task

--- tests/test_models/test_engine/test_file_storage.py
+++ tests/test_models/test_engine/test_file_storage.py
@@ -53,3 +53,27 @@ class TestFileStorage(unittest.TestCase):
         models.storage.save()
         fs.reload()
         self.assertIn("BaseModel." + bm.id, fs.all())
+
+class TestFileStorageInit(unittest.TestCase):
+    """Contains test cases against the FileStorage initialization"""
+
+    def test_file_path_is_a_private_class_attr(self):
+        """Checks that file_path is a private class attribute"""
+        self.assertFalse(hasattr(FileStorage(), "__file_path"))
+
+    def test_objects_is_a_private_class_attr(self):
+        """Checks that objects is a private class attribute"""
+        self.assertFalse(hasattr(FileStorage(), "__objects"))
+
+    def test_init_without_arg(self):
+        """Tests initialization without args"""
+        self.assertEqual(type(FileStorage()), FileStorage)
+
+    def test_init_with_arg(self):
+        """Tests initialization with args"""
+        with self.assertRaises(TypeError):
+            FileStorage(None)
+
+    def test_storage_initialization(self):
+        """Tests storage created in __init__.py"""
+        self.assertEqual(type(models.storage), FileStorage)
